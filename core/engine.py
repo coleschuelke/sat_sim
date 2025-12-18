bug in the gravity gradient

core/engine.py
@@ -40,7 +40,7 @@ class PhysicsEngine:
         control_torques = np.zeros(3)
 
         total_torques = env_torques + control_torques
-
+        # print(total_torques)
         # Dynamics
         v_dot = total_forces / body.mass
         omega_dot = np.linalg.inv(I) @ (total_torques - np.cross(omega, H))
@@ -48,6 +48,7 @@ class PhysicsEngine:
         # Kinematics
         r_dot = velocity
         q_dot = attitude.rate_of_change(omega)
+        # print(q_dot)
 
         # Derivative of state for integration
         dxdt = np.concatenate((r_dot, v_dot, q_dot, omega_dot))
@@ -79,6 +80,7 @@ class PhysicsEngine:
         final_velocity = final_state[3:6]
         final_attitude = final_state[6:10]
         final_angular_velocity = final_state[10:13]
+        print(final_attitude)
 
         body.position = final_positon
         body.velocity = final_velocity

environments/environment.py
@@ -81,9 +81,20 @@ class TwoBodyJ2(Environment):
         :param state: Description
         :param body: Description
         """
+        r_inertial = state[0:3]
+        attitude = Quaternion(*state[6:10])
+        I = body.inertia
+        mu = config.G * config.EARTH_MASS
+        r5 = np.linalg.norm(r_inertial) ** 5
+        r_body = attitude.conjugate().rotate_vector(r_inertial)
+
+        tau_gg = 3*mu * np.cross(r_body, I @ r_body) / r5
+
+        tau_total = tau_gg
         
-        # TODO: implement gravity gradient
-        return np.zeros(3)
+        # TODO: Drag
+
+        return tau_total
 
 
 class CR3BP(Environment):

main.py
@@ -24,11 +24,11 @@ def main():
     sat1 = Satellite(
         name='Sat1', 
         mass=500, 
-        inertia=np.diag([500, 500, 600]), 
+        inertia=np.diag([10, 10, 2]), 
         position=np.array((0, 0, 1500000 + cfig.EARTH_RADIUS)), 
         velocity=np.array((9000, 0, 0)),
-        attitude=Quaternion(1, 0, 0, 0),
-        angular_velocity=[0.003, 0.005, 0.001]
+        attitude=Quaternion(1, 2, 3, 4),
+        angular_velocity=[0.000, 0.000, 0.000]
     )
     sat2 = Satellite(
         name='Sat2', 
@@ -37,7 +37,7 @@ def main():
         position=np.array((0, 0, 400000 + cfig.EARTH_RADIUS)), 
         velocity=np.array((7800*np.cos(45), 7800*np.sin(45), 0)),
         attitude=Quaternion(1, 2, 3, 4),
-        angular_velocity=[0.003, 0.0, 0.001]
+        angular_velocity=[1e-6, 3e-6, -1e-6]
     )
 
     constellation = [sat1, sat2]